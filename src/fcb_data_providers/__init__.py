83d6d4dcad025c4a0ff97daa18ef57f015a97dc1
saudBinHabib <develop.android.apps01@gmail.com>
2024-12-03 17:55:15 +0100

    basic python code.

---
+++ src/fcb_data_providers/__init__.py
@@ -0,0 +1,11 @@
+"""
+fcb_data_providers
+
+This is a python package to handle the data providers for the scouting models
+
+"""
+
+__title__ = "FC Bayern Data Providers"
+__copyright__ = "© 2024 saud"
+
+from .version import __version__
